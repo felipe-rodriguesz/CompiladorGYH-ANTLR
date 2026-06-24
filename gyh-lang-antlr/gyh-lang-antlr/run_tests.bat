ef48b89dc83db3c7c8e583b4d645b44919370823
Felipe Rodrigues <feliperodrigues.2004@alunos.utfpr.edu.br>
2026-06-23 22:52:21 -0300

    Removendo scripts de teste do repositorio

--- gyh-lang-antlr/gyh-lang-antlr/run_tests.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-(for /L %%i in (1,1,19) do (
-    echo ==== TEST: programa%%i.gyh ====
-    call "C:\Program Files\Apache NetBeans\java\maven\bin\mvn.cmd" -q exec:java "-Dexec.args=programa%%i.gyh"
-)) > resultado_dos_testes.md 2>&1
